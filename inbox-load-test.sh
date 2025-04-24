c29f28ac4addda83f68c04d2602476af6a1ab64b
Nathan Santos <nathangar01@gmail.com>
2025-04-24 20:59:02 -0300

    adjusting autocannon

--- inbox-load-test.sh
+++ inbox-load-test.sh
@@ -9,4 +9,21 @@ npx autocannon -c 20 -d 10 -p 10 -m POST \
   }' \
   http://localhost:3000/inbox &
 
+npx autocannon -c 20 -d 10 -p 10 -m POST \
+  -H "Content-Type: application/json" \
+  -b '{
+    "type": "order_created",
+    "payload": {"customerName": "Jhon", "product": "Book"}
+  }' \
+  http://localhost:3000/inbox &
+
+
+npx autocannon -c 20 -d 10 -p 10 -m POST \
+  -H "Content-Type: application/json" \
+  -b '{
+    "type": "order_created",
+    "payload": {"customerName": "Karen", "product": "Book"}
+  }' \
+  http://localhost:3000/inbox &
+
 wait
